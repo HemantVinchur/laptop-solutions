c53d01fea32d5572b01351077e0227c7bb4915ca
HemantVinchur <hemantvinchur96@gmail.com>
2021-08-15 16:28:44 +0530

    First commit

---
+++ routes/landing.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var router = express.Router();
+
+/* GET add user page. */
+router.get("/", function (req, res, next) {
+  let payLoad = req.query.message;
+  res.render("landing", { message: payLoad });
+});
+
+module.exports = router;
